f2fa58fd1b5d3168f6a313b4e7b7805b99e4c05e
TomKwanten <tomkwanten2001@gmail.com>
2023-12-20 20:39:50 +0100

    paginatie

--- src/components/ProductCardComponent.vue
+++ src/components/ProductCardComponent.vue
@@ -1,79 +1,102 @@
 <template>
-    <div>
-      <section class="container-intro">
-        <h1>{{ beschrijving.titel }}</h1>
-        <p>{{ beschrijving.beschrijving }}</p>
-      </section>
-  
-      <form class="container-form" @submit.prevent="filterProducts">
-        <label for="product-type">Soort product:</label>
-        <select v-model="filter.productType" id="product-type">
-          <option value="alle">Alle</option>
-          <option value="hoedjes">Hoedjes en petten</option>
-          <option value="brillen">Rave brillen</option>
-          <option value="tasjes">Rave tasjes</option>
-          <option value="waaiers">Rave waaiers</option>
-        </select>
-  
-        <label for="product-color">Kleur:</label>
-        <select v-model="filter.productColor" id="product-color">
-          <option value="alle">Alle</option>
-          <option value="zwart">Zwart</option>
-          <option value="blauw">Blauw</option>
-          <option value="overig">Overige kleuren</option>
-        </select>
-  
-        <button type="submit">Filteren</button>
-      </form>
-  
-      <section class="container-product1">
-        <div v-for="product in filteredProducts" :key="product.id" class="product">
-          <a :href="'product.html?id=' + product.id" class="product-link">
-            <img :src="product.image" :alt="product.title">
-            <h3>{{ product.title }}</h3>
-            <span class="price">{{ product.price }}</span>
-          </a>
-        </div>
-      </section>
+  <div>
+    <section class="container-intro">
+      <h1>{{ beschrijving.titel }}</h1>
+      <p>{{ beschrijving.beschrijving }}</p>
+    </section>
+
+    <form class="container-form" @submit.prevent="filterProducts">
+      <label for="product-type">Soort product:</label>
+      <select v-model="filter.productType" id="product-type">
+        <option value="alle">Alle</option>
+        <option value="hoedjes">Hoedjes en petten</option>
+        <option value="brillen">Rave brillen</option>
+        <option value="tasjes">Rave tasjes</option>
+        <option value="waaiers">Rave waaiers</option>
+      </select>
+
+      <label for="product-color">Kleur:</label>
+      <select v-model="filter.productColor" id="product-color">
+        <option value="alle">Alle</option>
+        <option value="zwart">Zwart</option>
+        <option value="blauw">Blauw</option>
+        <option value="overig">Overige kleuren</option>
+      </select>
+
+      <button type="submit">Filteren</button>
+    </form>
+
+    <section class="container-product1">
+      <div v-for="product in paginatedProducts" :key="product.id" class="product">
+        <a :href="'product.html?id=' + product.id" class="product-link">
+          <img :src="product.image" :alt="product.title">
+          <h3>{{ product.title }}</h3>
+          <span class="price">{{ product.price }}</span>
+        </a>
+      </div>
+    </section>
+
+    <div class="pagination">
+      <button @click="changePage(-1)" :disabled="currentPage === 1">Vorige</button>
+      <span>{{ currentPage }}</span>
+      <button @click="changePage(1)" :disabled="currentPage === totalPages">Volgende</button>
     </div>
-  </template>
-  
-  <script>
-  import productsData from '../../products.json';
-  
-  export default {
-    data() {
-      return {
-        beschrijving: {
-          titel: "Welkom bij RaveWear",
-          beschrijving:
-            "Bij RaveWear begrijpen we de opwinding en vrijheid die gepaard gaan met het beleven van raves. Wij zijn trots om een uitgebreide online winkel voor ravers aan te bieden waar de nieuwste en de meest opwindende rave-kleding en accessoires te vinden zijn.",
-        },
-        filter: {
-          productType: "alle",
-          productColor: "alle",
-        },
-        products: productsData.products,
-      };
-    },
-    computed: {
-      filteredProducts() {
-        const filtered = this.products.filter((product) => {
-          const typeCondition =
-            this.filter.productType === "alle" || product.productType === this.filter.productType;
-          const colorCondition =
-            this.filter.productColor === "alle" || product.color.toLowerCase() === this.filter.productColor;
-  
-          return typeCondition && colorCondition;
-        });
-  
-        return filtered;
+  </div>
+</template>
+
+<script>
+import productsData from "../../products.json";
+
+export default {
+  data() {
+    return {
+      beschrijving: {
+        titel: "Welkom bij RaveWear",
+        beschrijving:
+          "Bij RaveWear begrijpen we de opwinding en vrijheid die gepaard gaan met het beleven van raves. Wij zijn trots om een uitgebreide online winkel voor ravers aan te bieden waar de nieuwste en de meest opwindende rave-kleding en accessoires te vinden zijn.",
       },
-    },
-    methods: {
-      filterProducts() {
-        // Voeg hier indien nodig extra logica toe voor het filteren
+      filter: {
+        productType: "alle",
+        productColor: "alle",
       },
+      products: productsData.products,
+      itemsPerPage: 8,
+      currentPage: 1,
+    };
+  },
+  computed: {
+    filteredProducts() {
+      const filtered = this.products.filter((product) => {
+        const typeCondition =
+          this.filter.productType === "alle" || product.productType === this.filter.productType;
+        const colorCondition =
+          this.filter.productColor === "alle" || product.color.toLowerCase() === this.filter.productColor;
+
+        return typeCondition && colorCondition;
+      });
+
+      return filtered;
     },
-  };
-  </script>
+    totalPages() {
+      return Math.ceil(this.filteredProducts.length / this.itemsPerPage);
+    },
+    paginatedProducts() {
+      const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+      const endIndex = startIndex + this.itemsPerPage;
+      return this.filteredProducts.slice(startIndex, endIndex);
+    },
+  },
+  methods: {
+    filterProducts() {
+      // Voeg hier indien nodig extra logica toe voor het filteren
+    },
+    changePage(value) {
+      this.currentPage += value;
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Voeg hier je stijlen toe */
+</style>
