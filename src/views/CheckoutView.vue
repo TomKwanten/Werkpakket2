fcc3036440787ab3156a6b2354144674dcb7dc99
TomKwanten <tomkwanten2001@gmail.com>
2023-12-21 22:27:27 +0100

    uitloggen

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,166 +1,165 @@
 <template>
-    <div>
-      <h2>Afrekenen</h2>
-      <div v-if="cart.length > 0">
-        <h3>Overzicht van uw bestelling</h3>
-        <!-- Productoverzicht -->
-        <ul>
-          <li v-for="item in cart" :key="item.id" class="checkout-item">
-            <div class="checkout-item-details">
-              <h4>{{ item.title }}</h4>
-              <p>Aantal: {{ item.quantity }}</p>
-            </div>
-            <div class="checkout-item-subtotal">
-              <p>Subtotaal: {{ formatCurrency(subtotal(item)) }}</p>
-            </div>
-          </li>
-        </ul>
-  
-        <!-- Adresgegevens formulier -->
-        <h3>Adresgegevens</h3>
-        <form @submit.prevent="submitOrder">
-          <!-- Naam, adres, stad, postcode inputvelden (gegevens voor verzending) -->
-            <label for="name">Naam:</label>
-            <input type="text" id="name" v-model="shippingInfo.name" required>
-
-            <label for="address">Adres:</label>
-            <input type="text" id="address" v-model="shippingInfo.address" required>
-
-            <label for="city">Stad:</label>
-            <input type="text" id="city" v-model="shippingInfo.city" required>
-
-            <label for="postalCode">Postcode:</label>
-            <input type="text" id="postalCode" v-model="shippingInfo.postalCode" required>
-
-  
-          <!-- Checkbox voor verschillende facturatiegegevens -->
-          <br>
-          <br>
-          <div class="checkbox-wrapper">
-            <input type="checkbox" id="differentBilling" v-model="differentBilling" @change="toggleBillingForm">
-            <span class="checkbox-label">Verschillend</span>
+  <div>
+    <h2>Afrekenen</h2>
+    <div v-if="cart.length > 0">
+      <h3>Overzicht van uw bestelling</h3>
+      <!-- Productoverzicht -->
+      <ul>
+        <li v-for="item in cart" :key="item.id" class="checkout-item">
+          <div class="checkout-item-details">
+            <h4>{{ item.title }}</h4>
+            <p>Aantal: {{ item.quantity }}</p>
           </div>
-  
-          <!-- Facturatiegegevens formulier (wordt alleen weergegeven als differentBilling actief is) -->
-          <div v-if="differentBilling">
-            <h3>Facturatiegegevens</h3>
-            <!-- Naam, adres, stad, postcode inputvelden (gegevens voor facturatie) -->
-            <label for="billingName">Naam:</label>
-            <input type="text" id="billingName" v-model="billingInfo.name">
-
-            <label for="billingAddress">Adres:</label>
-            <input type="text" id="billingAddress" v-model="billingInfo.address">
-
-            <label for="billingCity">Stad:</label>
-            <input type="text" id="billingCity" v-model="billingInfo.city">
-
-            <label for="billingPostalCode">Postcode:</label>
-            <input type="text" id="billingPostalCode" v-model="billingInfo.postalCode">
-       
+          <div class="checkout-item-subtotal">
+            <p>Subtotaal: {{ formatCurrency(subtotal(item)) }}</p>
           </div>
-  
-          <!-- Bestellingsoverzicht -->
-          <div class="checkout-total">
-            <p>Totale prijs: {{ formatCurrency(totalPrice) }}</p>
-            <p>Totale BTW (21%): {{ formatCurrency(totalPrice * 0.21) }}</p>
-  
-            <!-- Knop "Plaats bestelling" onder totale BTW -->
-            <button type="submit">Plaats bestelling</button>
-          </div>
-        </form>
-      </div>
-      <p v-else>Winkelmandje is leeg. Voeg producten toe om door te gaan met afrekenen.</p>
+        </li>
+      </ul>
+
+      <!-- Adresgegevens formulier -->
+      <h3>Adresgegevens</h3>
+      <form @submit.prevent="submitOrder">
+        <!-- Naam, adres, stad, postcode inputvelden (gegevens voor verzending) -->
+          <label for="name">Naam:</label>
+          <input type="text" id="name" v-model="shippingInfo.name" required>
+
+          <label for="address">Adres:</label>
+          <input type="text" id="address" v-model="shippingInfo.address" required>
+
+          <label for="city">Stad:</label>
+          <input type="text" id="city" v-model="shippingInfo.city" required>
+
+          <label for="postalCode">Postcode:</label>
+          <input type="text" id="postalCode" v-model="shippingInfo.postalCode" required>
+
+
+        <!-- Checkbox voor verschillende facturatiegegevens -->
+        <br>
+        <br>
+        <div class="checkbox-wrapper">
+          <input type="checkbox" id="differentBilling" v-model="differentBilling" @change="toggleBillingForm">
+          <span class="checkbox-label">Verschillend</span>
+        </div>
+
+        <!-- Facturatiegegevens formulier (wordt alleen weergegeven als differentBilling actief is) -->
+        <div v-if="differentBilling">
+          <h3>Facturatiegegevens</h3>
+          <!-- Naam, adres, stad, postcode inputvelden (gegevens voor facturatie) -->
+          <label for="billingName">Naam:</label>
+          <input type="text" id="billingName" v-model="billingInfo.name">
+
+          <label for="billingAddress">Adres:</label>
+          <input type="text" id="billingAddress" v-model="billingInfo.address">
+
+          <label for="billingCity">Stad:</label>
+          <input type="text" id="billingCity" v-model="billingInfo.city">
+
+          <label for="billingPostalCode">Postcode:</label>
+          <input type="text" id="billingPostalCode" v-model="billingInfo.postalCode">
+     
+        </div>
+
+        <!-- Bestellingsoverzicht -->
+        <div class="checkout-total">
+          <p>Totale prijs: {{ formatCurrency(totalPrice) }}</p>
+          <p>Totale BTW (21%): {{ formatCurrency(totalPrice * 0.21) }}</p>
+
+          <!-- Knop "Plaats bestelling" onder totale BTW -->
+          <button type="submit">Plaats bestelling</button>
+        </div>
+      </form>
     </div>
-  </template>
-  
-  <script>
-  import { useProductStore } from '../stores/productStore';
-  import { useRouter } from 'vue-router';
+    <p v-else>Winkelmandje is leeg. Voeg producten toe om door te gaan met afrekenen.</p>
+  </div>
+</template>
 
-  
-  export default {
-    data() {
-      return {
-        shippingInfo: {
+<script>
+import { useProductStore } from '../stores/productStore';
+import { useRouter } from 'vue-router';
+
+
+export default {
+  data() {
+    return {
+      shippingInfo: {
+        name: '',
+        address: '',
+        city: '',
+        postalCode: '',
+      },
+      differentBilling: false,
+      billingInfo: {
+        name: '',
+        address: '',
+        city: '',
+        postalCode: '',
+      },
+    };
+  },
+  computed: {
+    cart() {
+      return useProductStore().cart.map(item => ({
+        id: item.id,
+        title: item.title,
+        quantity: item.quantity,
+        price: item.price,
+      }));
+    },
+    totalPrice() {
+      return this.cart.reduce((total, item) => total + this.subtotal(item), 0);
+    },
+  },
+  methods: {
+    submitOrder() {
+      // Plaats hier de logica om de bestelling te verwerken, inclusief de adres- en facturatiegegevens
+      console.log('Bestelling geplaatst met adresgegevens:', this.shippingInfo);
+      console.log('Facturatiegegevens:', this.billingInfo);
+      // Reset het winkelmandje na het plaatsen van de bestelling
+      useProductStore().resetCart();
+      this.$router.push('/bevestiging');
+    },
+    toggleBillingForm() {
+      // Schakel het facturatiegegevens formulier in/uit op basis van de checkbox
+      if (!this.differentBilling) {
+        this.billingInfo = {
           name: '',
           address: '',
           city: '',
           postalCode: '',
-        },
-        differentBilling: false,
-        billingInfo: {
-          name: '',
-          address: '',
-          city: '',
-          postalCode: '',
-        },
-      };
+        };
+      }
     },
-    computed: {
-      cart() {
-        return useProductStore().cart.map(item => ({
-          id: item.id,
-          title: item.title,
-          quantity: item.quantity,
-          price: item.price,
-        }));
-      },
-      totalPrice() {
-        return this.cart.reduce((total, item) => total + this.subtotal(item), 0);
-      },
+    subtotal(item) {
+      return item.quantity * parseFloat(item.price.replace('€', '').replace(',', '.'));
     },
-    methods: {
-      submitOrder() {
-        // Plaats hier de logica om de bestelling te verwerken, inclusief de adres- en facturatiegegevens
-        console.log('Bestelling geplaatst met adresgegevens:', this.shippingInfo);
-        console.log('Facturatiegegevens:', this.billingInfo);
-        // Reset het winkelmandje na het plaatsen van de bestelling
-        useProductStore().resetCart();
-        this.$router.push('/bevestiging');
-      },
-      toggleBillingForm() {
-        // Schakel het facturatiegegevens formulier in/uit op basis van de checkbox
-        if (!this.differentBilling) {
-          this.billingInfo = {
-            name: '',
-            address: '',
-            city: '',
-            postalCode: '',
-          };
-        }
-      },
-      subtotal(item) {
-        return item.quantity * parseFloat(item.price.replace('€', '').replace(',', '.'));
-      },
-      formatCurrency(value) {
-        return value.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' });
-      },
+    formatCurrency(value) {
+      return value.toLocaleString('nl-NL', { style: 'currency', currency: 'EUR' });
     },
-  };
-  </script>
-  
-  <style scoped>
-  .checkbox-wrapper {
-    display: flex;
-    align-items: center;
-  }
-  
-  .checkbox-wrapper input {
-    margin-right: 8px;
-  }
-  
-  .checkbox-label {
-    font-weight: bold;
-  }
-  
-  /* Voeg wat extra ruimte toe onder de totale BTW */
-  .checkout-total {
-    margin-top: 20px;
-  }
-  
-  /* Voeg wat ruimte toe onder elk product in het overzicht */
-  .checkout-item {
-    margin-bottom: 10px;
-  }
-  </style>
-  
+  },
+};
+</script>
+
+<style scoped>
+.checkbox-wrapper {
+  display: flex;
+  align-items: center;
+}
+
+.checkbox-wrapper input {
+  margin-right: 8px;
+}
+
+.checkbox-label {
+  font-weight: bold;
+}
+
+/* Voeg wat extra ruimte toe onder de totale BTW */
+.checkout-total {
+  margin-top: 20px;
+}
+
+/* Voeg wat ruimte toe onder elk product in het overzicht */
+.checkout-item {
+  margin-bottom: 10px;
+}
+</style>
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,16 +1,22 @@
 <template>
     <div>
       <h2>Inloggen</h2>
-      <form @submit.prevent="login">
+      <p v-if="loggedInUser">Welkom terug, {{ loggedInUser }}! <button @click="logout">Uitloggen</button></p>
+      <!-- Toon inlogvelden als er geen ingelogde gebruiker is -->
+      <form v-if="!loggedInUser" @submit.prevent="login">
         <label for="username">Gebruikersnaam:</label>
         <input type="text" id="username" v-model="loginData.username" required>
   
         <label for="password">Wachtwoord:</label>
         <input type="password" id="password" v-model="loginData.password" required>
   
+        <label>
+          <input type="checkbox" v-model="rememberMe"> Onthoud mij
+        </label>
+  
         <button type="submit">Inloggen</button>
       </form>
-      <p v-if="loginError" style="color: red;">Foutieve gebruikersnaam of wachtwoord. Probeer opnieuw.</p>
+      <p v-if="loginError && !loggedInUser" style="color: red;">Foutieve gebruikersnaam of wachtwoord. Probeer opnieuw.</p>
     </div>
   </template>
   
@@ -23,11 +29,17 @@
           password: '',
         },
         loginError: false,
+        rememberMe: false,
+        loggedInUser: null,
         users: [
           { username: 'gebruiker1', password: 'wachtwoord1' },
           { username: 'gebruiker2', password: 'wachtwoord2' },
         ],
       };
+    },
+    created() {
+      // Controleer of de gebruiker is ingelogd op basis van localStorage
+      this.loggedInUser = localStorage.getItem('loggedInUser');
     },
     methods: {
       login() {
@@ -37,12 +49,25 @@
         if (user) {
           // Reset eventuele foutmelding
           this.loginError = false;
+  
+          // Onthoud de inlogstatus als de gebruiker dit wil
+          if (this.rememberMe) {
+            localStorage.setItem('loggedInUser', this.loginData.username);
+          }
+  
           // Navigeer naar het winkelmandje (pas de route aan naar de daadwerkelijke winkelmandroute)
           this.$router.push('/winkelmand');
         } else {
           // Geef een foutmelding weer als de inloggegevens niet overeenkomen
           this.loginError = true;
         }
+      },
+      logout() {
+        // Verwijder de inlogstatus en herlaad de pagina
+        localStorage.removeItem('loggedInUser');
+        this.loggedInUser = null;
+        // Herlaad de pagina om terug naar de lege inlogvelden te gaan
+        window.location.reload();
       },
     },
   };
