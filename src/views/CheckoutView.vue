e4956f21bb5aa4bc4ae5c878cff4d0f69e26c7a6
TomKwanten <tomkwanten2001@gmail.com>
2023-12-22 19:04:55 +0100

    checkout pagina klaar

--- src/views/CheckoutView.vue
+++ src/views/CheckoutView.vue
@@ -1,75 +1,63 @@
 <template>
-  <div>
-    <h2>Afrekenen</h2>
-    <div v-if="cart.length > 0">
-      <h3>Overzicht van uw bestelling</h3>
-      <!-- Productoverzicht -->
-      <ul>
-        <li v-for="item in cart" :key="item.id" class="checkout-item">
-          <div class="checkout-item-details">
+  <div class="checkout-container">
+    <h2 class="titel">{{ titels.hoofdtitel }}</h2>
+    <div class="checkout-item" v-if="cart.length > 0">
+      <h3>{{ titels.titel }}</h3>
+      <ul class="checkout-items">
+        <li class="checkout-item" v-for="item in cart" :key="item.id">
+          <div>
             <h4>{{ item.title }}</h4>
-            <p>Aantal: {{ item.quantity }}</p>
+            <p>{{ totaal.aantal }} {{ item.quantity }}</p>
           </div>
-          <div class="checkout-item-subtotal">
-            <p>Subtotaal: {{ formatCurrency(subtotal(item)) }}</p>
+          <div>
+            <p>{{ totaal.totaal }} {{ formatCurrency(subtotal(item)) }}</p>
           </div>
         </li>
       </ul>
 
-      <!-- Adresgegevens formulier -->
-      <h3>Adresgegevens</h3>
+      <h3>{{ invoervelden.titel }}</h3>
       <form @submit.prevent="submitOrder">
-        <!-- Naam, adres, stad, postcode inputvelden (gegevens voor verzending) -->
-          <label for="name">Naam:</label>
-          <input type="text" id="name" v-model="shippingInfo.name" required>
+        <label for="name">{{ invoervelden.naam }}</label>
+        <input type="text" id="name" v-model="shippingInfo.name" required>
 
-          <label for="address">Adres:</label>
-          <input type="text" id="address" v-model="shippingInfo.address" required>
+        <label for="address">{{ invoervelden.adres }}</label>
+        <input type="text" id="address" v-model="shippingInfo.address" required>
 
-          <label for="city">Stad:</label>
-          <input type="text" id="city" v-model="shippingInfo.city" required>
+        <label for="city">{{ invoervelden.stad }}</label>
+        <input type="text" id="city" v-model="shippingInfo.city" required>
 
-          <label for="postalCode">Postcode:</label>
-          <input type="text" id="postalCode" v-model="shippingInfo.postalCode" required>
-
-
-        <!-- Checkbox voor verschillende facturatiegegevens -->
+        <label for="postalCode">{{ invoervelden.postcode }}</label>
+        <input type="text" id="postalCode" v-model="shippingInfo.postalCode" required>
         <br>
         <br>
         <div class="checkbox-wrapper">
           <input type="checkbox" id="differentBilling" v-model="differentBilling" @change="toggleBillingForm">
-          <span class="checkbox-label">Verschillend</span>
+          <span class="checkbox-label">{{ invoervelden.verschil }}</span>
         </div>
 
-        <!-- Facturatiegegevens formulier (wordt alleen weergegeven als differentBilling actief is) -->
         <div v-if="differentBilling">
-          <h3>Facturatiegegevens</h3>
-          <!-- Naam, adres, stad, postcode inputvelden (gegevens voor facturatie) -->
-          <label for="billingName">Naam:</label>
+          <h3>{{ invoervelden.titel2 }}</h3>
+          <label for="billingName">{{ invoervelden.naam2 }}</label>
           <input type="text" id="billingName" v-model="billingInfo.name">
 
-          <label for="billingAddress">Adres:</label>
+          <label for="billingAddress">{{ invoervelden.adres2 }}</label>
           <input type="text" id="billingAddress" v-model="billingInfo.address">
 
-          <label for="billingCity">Stad:</label>
+          <label for="billingCity">{{ invoervelden.stad2 }}</label>
           <input type="text" id="billingCity" v-model="billingInfo.city">
 
-          <label for="billingPostalCode">Postcode:</label>
+          <label for="billingPostalCode">{{ invoervelden.postcode2 }}</label>
           <input type="text" id="billingPostalCode" v-model="billingInfo.postalCode">
-     
         </div>
 
-        <!-- Bestellingsoverzicht -->
-        <div class="checkout-total">
-          <p>Totale prijs: {{ formatCurrency(totalPrice) }}</p>
-          <p>Totale BTW (21%): {{ formatCurrency(totalPrice * 0.21) }}</p>
-
-          <!-- Knop "Plaats bestelling" onder totale BTW -->
-          <button type="submit">Plaats bestelling</button>
+        <div class="cart-total">
+          <p>{{ totaalprijs.prijs }} {{ formatCurrency(totalPrice) }}</p>
+          <p>{{ totaalprijs.btw }} {{ formatCurrency(totalPrice * 0.21) }}</p>
+          <button type="submit" class="confirm-button">{{ totaalprijs.knop }}</button>
         </div>
       </form>
     </div>
-    <p v-else>Winkelmandje is leeg. Voeg producten toe om door te gaan met afrekenen.</p>
+    <p class="leeg" v-else>{{ winkelmandleeg }}</p>
   </div>
 </template>
 
@@ -77,10 +65,35 @@
 import { useProductStore } from '../stores/productStore';
 import { useRouter } from 'vue-router';
 
-
 export default {
   data() {
     return {
+      titels: {
+        hoofdtitel: "Afrekenen",
+        titel: "Overzicht van uw bestelling",
+      },
+      totaal: {
+        aantal: "Aantal:",
+        totaal: "Subtotaal:",
+      },
+      invoervelden: {
+        titel: "Adresgegevens",
+        naam: "Naam:",
+        adres: "Adres:",
+        stad: "stad:",
+        postcode: "postcode:",
+        verschil: "Verschillend",
+        titel2: "Facturatiegegevens",
+        naam2: "Naam:",
+        adres2: "Adres:",
+        stad2: "stad:",
+        postcode2: "postcode:",
+      },
+      totaalprijs: {
+        prijs: "Totale prijs:",
+        btw: "Totale BTW (21%):",
+        knop: "Plaats bestelling",
+      },
       shippingInfo: {
         name: '',
         address: '',
@@ -94,6 +107,7 @@ export default {
         city: '',
         postalCode: '',
       },
+      winkelmandleeg: "Winkelmandje is leeg. Voeg producten toe om door te gaan met afrekenen.",
     };
   },
   computed: {
@@ -140,26 +154,59 @@ export default {
 </script>
 
 <style scoped>
-.checkbox-wrapper {
-  display: flex;
-  align-items: center;
+.leeg {
+  text-align: center;
+}
+.titel {
+  text-align: center;
+}
+.checkout-container {
+  max-width: 400px;
+  margin: auto;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
 }
 
 .checkbox-wrapper input {
   margin-right: 8px;
+  text-align: center;
 }
 
 .checkbox-label {
   font-weight: bold;
+  text-align: center;
+  align-items: center;
 }
-
-/* Voeg wat extra ruimte toe onder de totale BTW */
-.checkout-total {
+.cart-total {
   margin-top: 20px;
+  text-align: right;
+  font-weight: bold;
+  text-align: center;
 }
 
-/* Voeg wat ruimte toe onder elk product in het overzicht */
+.confirm-button {
+  margin-top: 20px;
+  background-color: #28a745;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.checkout-items {
+  justify-content: center;
+  text-align: center;
+  padding: 0;
+  width: 100%; 
+}
+
 .checkout-item {
-  margin-bottom: 10px;
+  justify-content: center;
+  text-align: center;
 }
 </style>
